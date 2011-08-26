81176a9a591db6ce34a1a90788e8356b1e9a5283
Burt Holzman <burt@fnal.gov>
2011-08-26 14:15:14 -0500

    Moved GitCommitNotifier::Emailer class methods back to instance methods

--- lib/git_commit_notifier/emailer.rb
+++ lib/git_commit_notifier/emailer.rb
@@ -14,28 +14,26 @@ class GitCommitNotifier::Emailer
     end
   end
 
-  class << self
-    def reset_template
-      @template = nil
-    end
+  def reset_template
+    @template = nil
+  end
 
-    def template
-      unless @template
-        source = IO.read(TEMPLATE)
-        begin
-          require 'erubis'
-           @template = Erubis::Eruby.new(source)
-        rescue LoadError
-          require 'erb'
-          @template = ERB.new(source)
-        end
+  def template
+    unless @template
+      source = IO.read(TEMPLATE)
+      begin
+        require 'erubis'
+        @template = Erubis::Eruby.new(source)
+      rescue LoadError
+        require 'erb'
+        @template = ERB.new(source)
       end
-      @template
     end
+    @template
   end
 
   def mail_html_message
-    html = GitCommitNotifier::Emailer.template.result(binding)
+    html = template.result(binding)
     premailer = Premailer.new(html, :with_html_string => true, :adapter => :nokogiri)
     premailer.to_inline_css
   end
